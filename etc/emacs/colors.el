01149fd033f990e365a7aba528f8e870d8cd654d
sezgin <sezdevx@gmail.com>
2019-01-28 23:27:28 -0500

    colors

--- etc/emacs/colors.el
+++ etc/emacs/colors.el
@@ -8,5 +8,5 @@
 (set-face-foreground 'font-lock-type-face "#A9B7C6")
 (set-face-foreground 'font-lock-builtin-face "#BBB529")
 (set-face-foreground 'font-lock-constant-face "#6897BB")
-(set-background-color black)
-(set-foreground-color white)
+(set-background-color "black")
+(set-foreground-color "white")
