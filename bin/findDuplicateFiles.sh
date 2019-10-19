5ce33e1438100f693d77f5979e552933423506a6
sezgin <sezdevx@gmail.com>
2019-10-19 10:48:02 -0400

    findDuplicateFiles made simpler, with a commented section that shows how to delete all files stored in a file

--- bin/findDuplicateFiles.sh
+++ bin/findDuplicateFiles.sh
@@ -3,8 +3,10 @@
 # finds all duplicate files in a given directory
 
 find "$*" -type f -exec md5sum {} +  > allDuplicateFiles.tmp
-sort allDuplicateFiles.tmp | uniq -d -w 32 | awk '{$1=""; print $0}' > duplicateFiles.tmp
+sort allDuplicateFiles.tmp | uniq -d -w 32 | awk '{$1=""; print $0}'
 \rm allDuplicateFiles.tmp
-cat duplicateFiles.tmp
-\rm duplicateFiles.tmp
 
+# below is how to remove all files in a file named duplicates.tmp
+#for f in $(cat duplicates.tmp) ; do
+#    rm "$f"
+#done
