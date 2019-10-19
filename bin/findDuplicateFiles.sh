0677a2ac6288fbfedb1b1fb97fa9b92e83b1fa11
sezgin <sezdevx@gmail.com>
2019-10-19 16:10:10 -0400

    improvements to findDuplicateFiles

--- bin/findDuplicateFiles.sh
+++ bin/findDuplicateFiles.sh
@@ -2,9 +2,20 @@
 
 # finds all duplicate files in a given directory
 
-find "$*" -type f -exec md5sum {} +  > allDuplicateFiles.tmp
-sort allDuplicateFiles.tmp | uniq -d -w 32 | awk '{$1=""; print $0}'
-\rm allDuplicateFiles.tmp
+tmpFile=$(mktemp /tmp/dedup.XXXXXXXXX)
+function finish()
+{
+    \rm -rf "$tmpFile"
+}
+trap finish EXIT
+
+if [[ "$ENV_PLATFORM" == "Mac" ]]; then
+    find "$*" -type f -exec md5 -r {} +  > "$tmpFile"
+else
+    find "$*" -type f -exec md5sum {} +  > "$tmpFile"
+fi
+sort "$tmpFile" | uniq -d -w 32 | awk '{$1=""; print $0}'
+
 
 # collect the output in duplicates.tmp and use the code below
 # to remove all files
