ae04bb5a8b3fc3b0bbc6eb07d82c55ea5f28fe12
Sezgin <sezdevx@gmailcom>
2019-10-20 19:21:30 +0000

    findOpenPorts permissions

--- bin/findOpenPorts.sh
+++ bin/findOpenPorts.sh
@@ -8,7 +8,7 @@ if [[ "$ENV_PLATFORM" == "Mac" ]]; then
     if [ `command -v lsof` ]; then
         sudo lsof -i -P -n | grep LISTEN
     elif [ `command -v netstat` ]; then
-        netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print colred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
+        netstat -Watnlv | grep LISTEN
     fi
 else
     if [ `command -v lsof` ]; then
@@ -16,6 +16,6 @@ else
     elif [ `command -v lsof` ]; then
         sudo lsof -i -P -n | grep LISTEN
     elif [ `command -v netstat` ]; then
-        netstat -Watnlv | grep LISTEN | awk '{"ps -o comm= -p " $9 | getline procname;colred="\033[01;31m";colclr="\033[0m"; print colred "proto: " colclr $1 colred " | addr.port: " colclr $4 colred " | pid: " colclr $9 colred " | name: " colclr procname;  }' | column -t -s "|"
+        netstat -Watnlv | grep LISTEN
     fi
 fi
